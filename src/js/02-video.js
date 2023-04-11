1736c51fd38df89611be7c2695d42fa105580f5a
Vadym Shatnii <vadymshatniy@gmail.com>
2023-04-11 20:04:50 +0300

    task 02 - video - 1
    without throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
 
+    player.on('play', function() {
+        localStorage.getItem('videoplayer-current-time', currentTime);
+    });
+
+    player.getVideoTitle().then(function(currentTime) {
+        localStorage.setItem('videoplayer-current-time', currentTime);
+    });
+
+
+
+
+    
