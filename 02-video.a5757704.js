73f5796b02c7f1cc382fbec10ccc46f6ea14c04c
vadymshatniy <118130400+vadymshatniy@users.noreply.github.com>
2023-04-12 19:59:09 +0000

    Deploying to gh-pages from @ vadymshatniy/goit-js-hw-08@b1ef95fdec7d446d515817210921d9736bf5f633 🚀

--- 02-video.a5757704.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e),o=document.getElementById("vimeo-player");t.on("play",(function(){localStorage.setItem("videoplayer-current-time",JSON.stringify(o.currentTime)),console.log(JSON.stringify(o.currentTime))})),t.getVideoTitle().then((function(e){localStorage.getItem("videoplayer-current-time")}));
-//# sourceMappingURL=02-video.a5757704.js.map
