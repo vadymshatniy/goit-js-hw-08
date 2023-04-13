6f30477f8cdc3b55afc008b0c64b9e28c6933f7d
Vadym Shatnii <vadymshatniy@gmail.com>
2023-04-13 21:21:13 +0300

    feedback 1
    feedback without throttle

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+// Призначення елементів DOM у змінні:
+const fieldEmail = document.querySelector('input');
+const fieldMessage = document.querySelector('textarea');
+const button = document.querySelector("button");
 
+// Запис даних з полів email і message у local Storage:
+function setDatasToLocal() {
+     localStorage.setItem('feedback-form-state', JSON.stringify({
+        email: fieldEmail.value,
+        message: fieldMessage.value,
+    }))
+}
+fieldEmail.addEventListener('input', () => {
+    setDatasToLocal();
+});
+fieldMessage.addEventListener('input', () => {
+    setDatasToLocal();
+});
+
+
+// Заповнення полів email і message даними з local Storage під час перезавантаження сторінки:
+const localDatas = JSON.parse(localStorage.getItem('feedback-form-state'));
+const localEmail = localDatas.email;
+const localMessage = localDatas.message;
+fieldEmail.value = localEmail;
+fieldMessage.value = localMessage;
+
+// Дії під час submit:
+button.addEventListener('click', () => {
+    localStorage.clear();
+    console.log({
+        email: fieldEmail.value,
+        message: fieldMessage.value,
+    });
+});
